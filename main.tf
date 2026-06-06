7ed623a1b2e9e8946fd2156044f7312be0682219
Garv Iyengar <garviyengar@gmail.com>
2026-06-06 21:53:58 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,66 @@
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name = "web_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance (Original)
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "Hello from Terraform EC2" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "Original-Instance"
+  }
+}
+
+# AMI from EC2
+resource "aws_ami_from_instance" "my_ami" {
+  name               = "terraform-ami"
+  source_instance_id = aws_instance.web.id
+}
+
+# New EC2 from AMI
+resource "aws_instance" "clone" {
+  ami           = aws_ami_from_instance.my_ami.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "Cloned-Instance"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {}
+variable "ami" {
+    default = "ami-0f5ee92e2d63afc18"
+}
+variable "instance_type" {}
+variable "key_name" {
+    default = "garvpc2"
+}
